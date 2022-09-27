91a6c327b3b143cd17ed8d7df99eb1962dd325b5
Alex Gicimo Gakuru <gakurualexgicimo@gmail.com>
2022-09-27 12:11:37 +0300

    Coding along react state and events code along

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,15 @@
 import React from "react";
+import {useState} from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+const[switchStatus,setSwitchStatus]=useState(false);
+
+function toggleSwitch(){
+setSwitchStatus((switchStatus)=>!switchStatus);
+}
+ const color=switchStatus?"red":"white";
+
+  return <button onClick={toggleSwitch} style={{background:color}}>{switchStatus?"ON":"OFF"}</button>;
 }
 
 export default Toggle;
